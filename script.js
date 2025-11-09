af22d11be5ce443ca401d88a96983a1794b6ccc5
Jane Ndafia <ndafiajane@gmail.com>
2025-11-09 05:18:23 +0100

    simpleCalculator

---
+++ script.js
@@ -0,0 +1,24 @@
+const display = document.getElementById("display");
+const buttons = document.querySelectorAll(".btn");
+const clear = document.getElementById("clear");
+const equal = document.getElementById("equal");
+
+buttons.forEach(button => {
+  button.addEventListener("click", () => {
+    const value = button.textContent;
+
+    if (value === "C") {
+      display.value = "";
+    } else if (value === "←") {
+      display.value = display.value.slice(0, -1);
+    } else if (value === "=") {
+      try {
+        display.value = eval(display.value) || "";
+      } catch {
+        display.value = "Error";
+      }
+    } else if (value !== "=") {
+      display.value += value;
+    }
+  });
+});
